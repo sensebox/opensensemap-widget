4c9e2e06f71ad05c5dc6ccf0d2096f0904396a44
Matthias Pfeil <matthias.pfeil@wwu.de>
2017-06-23 09:55:03 +0200

    fix path

--- deploy.sh
+++ deploy.sh
@@ -52,7 +52,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in /home/travis.ssh/super_secret.txt.enc -out super_secret.txt -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in /home/travis/.ssh/super_secret.txt.enc -out super_secret.txt -d
 ls -la
 chmod 600 super_secret.txt
 eval `ssh-agent -s`
