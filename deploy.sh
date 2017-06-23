b077de2d4615ae48979ac3f139cc6bd22424f3ab
Matthias Pfeil <matthias.pfeil@wwu.de>
2017-06-23 10:00:56 +0200

    pipe ssh key

--- deploy.sh
+++ deploy.sh
@@ -52,11 +52,8 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in /home/travis/.ssh/super_secret.txt.enc -out super_secret.txt -d
-ls -la
-chmod 600 super_secret.txt
 eval `ssh-agent -s`
-ssh-add super_secret.txt
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in /home/travis/.ssh/super_secret.txt.enc -d | ssh-add -
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
